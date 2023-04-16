accece6c17c99a04057dcee97c6b45f01e046e38
AndreD4 <andremai629@gmail.com>
2023-04-16 13:26:15 +0900

    Switch case for on collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,6 +4,19 @@ public class CollisionHandler : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-      
+      switch (other.gameObject.tag)
+      {
+        case "Friendly":
+            Debug.Log ("this thing is friendly");
+            break;
+
+        case "Finish":
+            Debug.Log ("yay you did it");
+            break;
+        default:
+            Debug.Log ("you died");
+            break;
+      }
     }
+    
 }
