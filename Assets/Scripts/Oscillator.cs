6c3bd36297d0103f1661874bae942ed7b0fc53ea
AndreD4 <andremai629@gmail.com>
2023-04-19 07:57:38 +0900

    Oscillator creating cycles

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,9 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+
     
     void Start()
     {
@@ -16,7 +18,15 @@ public class Oscillator : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        float cycles = Time.time / period; 
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSinWave + 1f) / 2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
     }
 }
