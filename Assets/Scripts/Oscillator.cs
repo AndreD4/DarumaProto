e9435fb94947fe19765428a5052dec2b0616b993
AndreD4 <andremai629@gmail.com>
2023-04-19 07:46:48 +0900

    Oscillator script for moveable objects

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float movementFactor;
+    
+    void Start()
+    {
+        startingPosition = transform.position;
+        Debug.Log(startingPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
